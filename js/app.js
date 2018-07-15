16a72806553a9a5b00d62f8f0ee48a50361b9506
Károly Zsolt Krekács <krekacs.k@outlook.com>
2018-07-15 09:59:44 +0200

    Completed 1st Task: Sort ships by cost_in_credits using adv. Bubblesort

--- js/app.js
+++ js/app.js
@@ -1,4 +1,29 @@
 // ide deklaráljátok a függvényeket.
+function getNumberPropertyValue(parameterItem, parameterProperty) {
+  if (typeof parameterItem[parameterProperty] === 'string') {
+    return parseInt(parameterItem[parameterProperty], 10);
+  }
+  return 0;
+}
+
+function getShipsOrderedByNumberProperty(parameterArray, parameterProperty) {
+  var tempArray = parameterArray.slice();
+  var endIndex = tempArray.length - 1;
+  var newEndIndex;
+  while (endIndex > 1) {
+    newEndIndex = 0;
+    for (var i = 0; i < endIndex; i++) {
+      if (getNumberPropertyValue(tempArray[i], parameterProperty) >
+      getNumberPropertyValue(tempArray[i + 1], parameterProperty)) {
+        [tempArray[i], tempArray[i + 1]] = [tempArray[i + 1], tempArray[i]];
+        newEndIndex = i;
+      }
+    }
+    endIndex = newEndIndex;
+  }
+
+  return tempArray;
+}
 
 function getData(url, callbackFunc) {
   var xhttp = new XMLHttpRequest();
@@ -15,6 +40,5 @@ function successAjax(xhttp) {
   // Innen lesz elérhető a JSON file tartalma, tehát az adatok amikkel dolgoznod kell
   var userDatas = JSON.parse(xhttp.responseText);
   // Innen lehet hívni.
-  console.log(userDatas);
 }
 getData('/json/spaceships.json', successAjax);
