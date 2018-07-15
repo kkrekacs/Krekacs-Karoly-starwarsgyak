d7699a2e3555056ab66894618107446d59241007
Károly Zsolt Krekács <krekacs.k@outlook.com>
2018-07-15 12:13:19 +0200

    Completed 4th Task: Show spacehip list in HTML.

--- js/app.js
+++ js/app.js
@@ -53,10 +53,41 @@ function setNullPropertiesToUnknown(parameterArray) {
   return tempArray;
 }
 
+function getSingleShipToDiv(parameterObject, objectIndex) {
+  var newShipDiv = document.createElement('div');
+  newShipDiv.className = `ship spacehip${objectIndex}`;
+  var newShipPropertiesDiv = document.createElement('div');
+  newShipPropertiesDiv.className = 'properties';
+  var newShipPropertyValuesDiv = document.createElement('div');
+  newShipPropertyValuesDiv.className = 'values';
+  var newShipImageDiv = document.createElement('div');
+  newShipImageDiv.className = 'image';
+  var objectKeys = Object.keys(parameterObject);
+  for (var i = 0; i < objectKeys.length - 1; i++) {
+    newShipPropertiesDiv.innerHTML += `${objectKeys[i]}:<br>`;
+    newShipPropertyValuesDiv.innerHTML += `${parameterObject[objectKeys[i]]}<br>`;
+  }
+  newShipImageDiv.innerHTML +=
+  `<img src='/img/${parameterObject[objectKeys[objectKeys.length - 1]]}' onerror="this.src='/img/obiwan.jpg'">`;
+  newShipDiv.appendChild(newShipPropertiesDiv);
+  newShipDiv.appendChild(newShipPropertyValuesDiv);
+  newShipDiv.appendChild(newShipImageDiv);
+
+  return newShipDiv;
+}
+
+function setMainShipList(parameterArray) {
+  var mainShipList = document.querySelector('.spaceship-list');
+  for (var i = 0; i < parameterArray.length; i++) {
+    mainShipList.appendChild(getSingleShipToDiv(parameterArray[i], i));
+  }
+}
+
+
 function getData(url, callbackFunc) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
+    if (this.readyState === 4 && this.status === 200) {
       callbackFunc(this);
     }
   };
@@ -68,5 +99,12 @@ function successAjax(xhttp) {
   // Innen lesz elérhető a JSON file tartalma, tehát az adatok amikkel dolgoznod kell
   var userDatas = JSON.parse(xhttp.responseText);
   // Innen lehet hívni.
+  setMainShipList(
+    setNullPropertiesToUnknown(
+      doRemoveObjectsWithNullConsumables(
+        getShipsOrderedByNumberProperty(userDatas, 'cost_in_credits')
+      )
+    )
+  );
 }
 getData('/json/spaceships.json', successAjax);
